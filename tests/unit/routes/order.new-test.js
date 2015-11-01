0b0d7a656a24cd5fdb12db6433a8b39f9a7256e2
Marcel van Pinxteren <marcel.van.pinxteren@gmail.com>
2015-11-01 17:29:43 +0100

    Initial commit

---
+++ tests/unit/routes/order.new-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:order.new', 'Unit | Route | order.new', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
